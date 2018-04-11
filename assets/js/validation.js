d4190678f2237432218dbea6949b5906a2772be3
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-04-12 00:34:28 +0300

    Paprastas JavaScript reaguojantis į įvestą laukelį
    Pridedame validacijos failą tik People puslapyje.
    
    Kodo sugeneruojamas (nukopijuojamas į build katalogą) node konteineryje:
      yarn run encore dev --watch
    
    Patikrinimui, ar veikia:
      http://127.0.0.1:8000/people keičiant teksta su klaviatūra, turi šalia rodyti
    
    (cherry picked from commit a312261)

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,1 +1,10 @@
 // Here will be validation in Frontend
+
+let name = document.getElementById('name');
+let validationResult = document.getElementById('validation-result');
+const validateName = function () {
+    validationResult.innerText = 'Įrašėte: ' + name.value;
+};
+
+name.onkeyup = validateName;
+name.onchange = validateName;
