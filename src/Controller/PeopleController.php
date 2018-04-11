8f15edefa7a181dcd07f9e0367f6121987ed14b6
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-04-12 00:48:54 +0300

    Pasidarome prieigą AJAX užklausoms (sudėtingesnis takų pavyzdys)
    Symfony turi galingą takų (angl. route) sistemą,
    tai galima perduoti argumentus užklausoje, kaip argumentus į funkciją.
    
    Pasidarome apribojimą, kad būtų galima kviesti tik per POST metodą.
    
    Dokumentacija:
      https://symfony.com/doc/current/bundles/SensioFrameworkExtraBundle/annotations/routing.html

--- src/Controller/PeopleController.php
+++ src/Controller/PeopleController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PeopleController extends Controller
 {
@@ -15,5 +18,19 @@ class PeopleController extends Controller
         return $this->render('people/index.html.twig', [
             'controller_name' => 'PeopleController',
         ]);
+    }
+
+    /**
+     * @Route("/validate/{element}", name="validatePerson")
+     * @Method({"POST"})
+     */
+    public function validate($element)
+    {
+        switch ($element) {
+            case 'Name':
+                return new JsonResponse(['valid' => true]);
+        }
+
+        return new JsonResponse(['error' => 'Invalid method'], Response::HTTP_BAD_REQUEST);
     }
 }
