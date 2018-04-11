163a0110db0eb67bbb81b0683460b2a78d24ca9b
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-04-12 00:03:03 +0300

    Perduokime duomenis iš Twig'o į JavaScript'ą
    Problema: naudojame React ir axious kodą tik People puslapyje, bet JavaScript įkraunamas visur. Taigi Pagrindiniame puslapyje Console'je metamos klaidos.
    Sprendimas: įterpti JavaScript dalį, kuri būtų virš. Naudojami globalūs JavaScript kintamieji ("var", bet ne "let")
    
    Gerosios praktikos: naudoti data-... parametrus
    Geresnis sprendimas: patikrinti ar elementas egzistuoja
    
    Kodas sugeneruotas naudojant node konteineryje:
      yarn run encore dev --watch
    
    Patikrinimui, ar veikia:
      Tiek pagrindiniame, tiek /people puslapyje Naršyklės Console nerodo klaidos
    
    Dokumentacija:
      https://symfony.com/doc/current/frontend/encore/server-data.html
      https://www.w3schools.com/js/js_datatypes.asp

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
 require('bootstrap');
 const axios = require('axios');
 
-let versionElement = document.getElementById('version');
-axios.get('/build/version.json')
-    .then(function(response) {
-        versionElement.innerText = response.data.date;
-    })
-    .catch(function (error) {
-        versionElement.innerText = 'Error: ' . error;
-    });
+if (typeof needVersion !== "undefined") {
+    let versionElement = document.getElementById('version');
+    axios.get('/build/version.json')
+        .then(function(response) {
+            versionElement.innerText = response.data.date;
+        })
+        .catch(function (error) {
+            versionElement.innerText = 'Error: ' . error;
+        });
+}
--- assets/js/react.jsx
+++ assets/js/react.jsx
@@ -1,7 +1,9 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-);
+if (typeof needVersion !== "undefined") {
+    ReactDOM.render(
+        <h1>Hello, world!</h1>,
+        document.getElementById('root')
+    );
+}
