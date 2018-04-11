f8b75427b3d6668e151c9bc1514f38859e8e6588
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-04-11 23:43:52 +0300

    Padarom AJAX užklausą naudojant axios biblioteką
    Čia viena iš pavyzdžių, kaip galima panaudoti išorinę bilbioteką,
    kad mūsų kodas būtų paprastesnis.
    
    Kad kodas veiktų, paleidžiame node konteineryje:
      yarn run encore dev --watch
    
    Patirkinimui, atsidarome Naršyklės console langą
      Turėtų būti kažkas į Object { date: "2018-04-11T20:31:23.279Z" }
    
    Dokumentacija:
      https://www.npmjs.com/package/axios
    
    (cherry picked from commit 8e80b6c)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,10 @@
 require('bootstrap');
 const axios = require('axios');
+
+axios.get('/build/version.json')
+    .then(function(response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.error(error);
+    });
