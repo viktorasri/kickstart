bcf89e774921aa95ed6d2cc65d27344e0aa6ca99
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-04-12 17:48:58 +0300

    Ištaisome klaidą

--- assets/js/react.jsx
+++ assets/js/react.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-if (typeof needVersion !== "undefined") {
+if (typeof needReact !== "undefined") {
     ReactDOM.render(
         <h1>Hello, world!</h1>,
         document.getElementById('root')
