32c6fc0c5d7bb18de85c8f1e683ec06e697d3b92
Aurelijus Banelis <aurelijus@banelis.lt>
2018-04-14 23:44:50 +0300

    Pridedam Security biblioteką
    Išnaudokime Symfony teikiamas galimybes naudotojų atpažinomui ir teisių valdymui.
    
    Kodas sugeneruotas įvykdžius:
      composer require security
    
    Dokumentacija:
      https://symfony.com/doc/current/security.html

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
