62e3dacaa78c6e09ab221db4716195e47bc47073
Viktoras <viktoras.rimkus@gmail.com>
2018-04-25 00:37:51 +0300

    registration redirect route change

--- src/EventSubscriber/RegistrationRedirectSubscriber.php
+++ src/EventSubscriber/RegistrationRedirectSubscriber.php
@@ -1,20 +1,31 @@
 <?php
-
 namespace App\EventSubscriber;
-
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\FOSUserEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
 class RegistrationRedirectSubscriber implements EventSubscriberInterface
 {
-    public function onFosUserRegistrationSuccess($event)
+    private $router;
+    public function __construct(RouterInterface $router)
     {
-        // ...
+        $this->router = $router;
     }
-
+    public function onFosUserRegistrationSuccess(FormEvent $event)
+    {
+        $url = $this->router->generate('admin');
+        $respononse = new RedirectResponse($url);
+        $event->setResponse($respononse);
+    }
+    /**
+     * @see RegistrationController::registerAction()
+     * @return array
+     */
     public static function getSubscribedEvents()
     {
         return [
-           'fos_user.registration.success' => 'onFosUserRegistrationSuccess',
+            FOSUserEvents::REGISTRATION_SUCCESS => 'onFosUserRegistrationSuccess',
         ];
     }
 }
